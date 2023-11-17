83955418c0ffb94ee19b644cadb4d8a565d9a181
Aduwo Ayooluwa <aduwoayooluwa@gmail.com>
2023-11-18 00:00:30 +0100

    ft/landing page

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,14 +1,6 @@
 import React from 'react'
 
-export const doctorProfile = {
-  id: 1,
-  name: 'Dr. Jane Smith',
-  imageUrl: 'https://images.unsplash.com/photo-1494790108377-be9c29b29330?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-  bio: 'Dr. Smith is an experienced Pediatrician with over 10 years of practice. She specializes in child healthcare and is committed to providing compassionate care to her young patients.',
-  rating: 4.7,
-  isOnline: true, 
-  specialty: 'Pediatrician'
-};
+
 
 
 type Props = {}
