38d0cccbae1841066e55a0802e54023a8bb7fa2c
Aduwo Ayooluwa <aduwoayooluwa@gmail.com>
2023-11-18 00:04:18 +0100

    ft/landing page

--- src/user/addons/Doctors.tsx
+++ src/user/addons/Doctors.tsx
@@ -10,7 +10,7 @@ import DoctorProfile from './DocProfile';
 import { useGetDoctors } from '@/helper/users';
 
 
-export const doctorProfile = {
+const doctorProfile = {
   id: 1,
   name: 'Dr. Jane Smith',
   imageUrl: 'https://images.unsplash.com/photo-1494790108377-be9c29b29330?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
